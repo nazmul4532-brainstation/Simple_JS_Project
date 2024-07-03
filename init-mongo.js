308c319079af03684e56d776ba8c202a82271bd6
Nazmul Hossain <mm.nazmul@brainstation-23.com>
2024-07-03 20:10:13 +0600

    Reconfigured init-mongo and the mongo.yml files

--- init-mongo.js
+++ init-mongo.js
@@ -1,10 +1,7 @@
-// Switch to the 'user-account' database
 db = db.getSiblingDB('user-account');
 
-// Create the 'users' collection
 db.createCollection('users');
 
-// Create the 'admin' user with the specified roles
 db.createUser({
   user: 'admin',
   pwd: 'password',
@@ -14,7 +11,6 @@ db.createUser({
   ]
 });
 
-// Insert the initial document into the 'users' collection
 db.users.insertOne({
   userId: 1,
   email: 'mm.nazmul@brainstation-23.com',
